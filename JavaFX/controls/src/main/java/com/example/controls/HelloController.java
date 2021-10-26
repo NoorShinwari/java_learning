1676c2adf9d16569277da98a75c283780c112a2e
Noor Ullah <noor@togometrics.net>
2021-10-26 16:33:27 +0200

    commit

--- JavaFX/controls/src/main/java/com/example/controls/HelloController.java
+++ JavaFX/controls/src/main/java/com/example/controls/HelloController.java
@@ -1,14 +1,42 @@
 package com.example.controls;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextField textToPrint;
+
+
+    @FXML
+    private Button clickMeButton;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    public  void initialize(){
+        clickMeButton.setDisable(true);
+    }
+
+    @FXML protected  void printValueHandler(ActionEvent e){
+        System.out.println("This has been typed " + textToPrint.getText());
+        System.out.println("this is event " + e.getSource());
+
+    }
+
+    @FXML
+    public void handleKeyReleased(){
+        String text= textToPrint.getText();
+        boolean disableButton = text.isEmpty() || text.trim().isEmpty();
+        clickMeButton.setDisable(disableButton);
+
     }
 }
